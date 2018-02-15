3abeef1df0324a007a0cad6e14ca642406c97a00
Ian Spielman <spielman@umd.edu>
2018-02-15 12:50:24 -0500

    Updated conveniance to have Dimi's copy paste code.
    do "from labscript_utils.comvenience import copy2clipboard"
    
    call signature is: copy2clipboard(fig=None, resize=1, pix_around=5)

--- convenience.py
+++ convenience.py
@@ -7,6 +7,59 @@ Created on Fri Jun 19 09:16:29 2015
 Contains simple utilities that many functions can call
 """
 
+from __future__ import division, print_function 
+import matplotlib.pyplot as plt 
+from cStringIO import StringIO 
+from time import sleep 
+from PIL import Image 
+import win32clipboard 
+import numpy as np 
+ 
+# TODO import to pillow? 
+# TODO crop around axis 
+ 
+def copy2clipboard(fig=None, resize=1, pix_around=5): 
+    print(type(fig)) 
+    if not fig: 
+        fig = plt.gcf() 
+        ax = plt.gca() 
+    else: 
+        ax = fig.axes[0] 
+ 
+    fig.set_facecolor('white') 
+ 
+    output = StringIO() 
+    fig.canvas.draw() 
+    buf = fig.canvas.buffer_rgba() 
+    size = fig.canvas.size() 
+    w, h = size.width(), size.height() 
+    im = Image.frombuffer('RGBA', (w, h), buf, 'raw', 'RGBA', 0, 1) 
+ 
+    # crop canvas and give it some breathing space 
+    crop_size = np.array(ax.get_tightbbox(fig.canvas.renderer).bounds, dtype=int) 
+    crop_size[2:] += crop_size[:2] 
+    crop_size[:2] -= pix_around 
+    crop_size[2:] += 2*pix_around 
+    crop_size[crop_size < 0] = 0 
+    # TODO correct croping 
+ 
+    # now resize cropped image respecting aspect_ratio 
+    aspect_ratio = w*1.0/h 
+    new_size = (crop_size[2:]*np.array([1, aspect_ratio])*resize).astype(int) 
+ 
+    im.convert('RGB').crop(crop_size).resize(new_size, Image.LANCZOS).save(output, 'BMP') 
+    data = output.getvalue()[14:]  # drop BMP header 
+    output.close() 
+ 
+    try: 
+        win32clipboard.OpenClipboard() 
+        win32clipboard.EmptyClipboard() 
+        win32clipboard.SetClipboardData(win32clipboard.CF_DIB, data) 
+        win32clipboard.CloseClipboard() 
+    except: 
+        sleep(0.2) 
+        copy2clipboard(fig) 
+
 def ValidName(name, RaiseError=False):
     try:
         # Test that name is a valid Python variable name:
@@ -18,3 +71,4 @@ def ValidName(name, RaiseError=False):
 
         return False
     return True
+
